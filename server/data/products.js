a54645d9a26e18f3eeed01522eec417ec4618bec
Prodaaron <aaronmesfin60@gmail.com>
2025-01-08 18:29:18 +0300

    customized and finished adding info on products

--- server/data/products.js
+++ server/data/products.js
@@ -2,7 +2,7 @@ const product = [
     {
         productId: 1,
         productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
+        productName: 'Blue Bead - Caramel flavored toping',
         productAvailability: 50,
         productDescription: {
             title: 'Test Product Description',
@@ -12,9 +12,9 @@ const product = [
     },
 
     {
-        productId: 1,
-        productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
+        productId: 2,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACj8A0LI37LtzNhqwbzQIaCvcKQEuvfHMAqFbUWpRpANLNWc6m4J5uk9Ntjwv0WQd5BDGvevhjJ0bF8VECGxchWSMrnHMxAfzgqbwmINflyqQzkwUJ_aLNAuwP7nX2FajhWm-GTnEfUjBhQAXxRVC5QG5QNMsPDxS8Q2gO7CAIZHt7OOAydO4wkTBMS1XGg0U6b26kZs9HbMXdeNLHzGB-njomJvN1qBVhTOEYPf-dTf_HiEFo1X9EDyufb1nQZKrNahqthxN-WAMoeCGpp1yG-WCIiO1qeL0MjTvUszM5U09kZtJaV_Xm6IMQZpVJ_ques/p.png',
+        productName: 'Blue Bead - Chocolate flavored toping',
         productAvailability: 50,
         productDescription: {
             title: 'Test Product Description',
@@ -24,9 +24,21 @@ const product = [
     },
 
     {
-        productId: 1,
-        productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
+        productId: 3,
+        productImage: 'https://previews.dropbox.com/p/thumb/AChqgTQI7ITdyBi__ElDSQOAbNjRBX3tBDgDL2TbJVS0l2VFRjCfe_CmtMSWtUGTWIbQBeaNTRFaoLXoZSRb96sOU6a7-XPFLFVNEnHIEJihMShflVFJob55x3Hzokjyk5jHgSKwmD5rxvKaQyPVA2sOpRiCPWZHIdwM7sBEW7-LB1JJ5wmOENiXQv-G3T2bFBxRowxp-4S_ZdZCBUh1aLCNFbcsxQFDx16JoCJIf7CghfiZmINGYfe4ssD0I85bvGZMwfZGussC-90_n5hudsYPLaP0hHxKmI-N0alg99MQEc2rOOdkOe-cyJCBgUofE-0/p.png',
+        productName: 'Blue Bead - Cold Glaze caramel flavored',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Blue Bead - Cold Glaze caramel flavored',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 4,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACiChWwgM3K7d7_j4bH8V-hDas9Rhd7iGDHW4j8gLthPDC93FcByDK4iqh72lY8LD1-vqu0jcJxBF80L9x6o-AJeiFYOa5RDkAydsIofLPWYEIZ_FWIBcxDzZ1YS8E23h6T7cyi1MHblK0daK5Ie6MthJAIzAwsGeIq7_UBH300vypsTFMwHOlYk3h0u5kH4bMAWxPWwNzlBPEV6upRLfdR4sWSZzcJUatqAdJrGSOaiC8gVO7RyxBbmrV8SSJbemWLEW7dALdZfmSjReNdVO-BXNzAEFkMFX9BZ476fWXxbwQF3LKpymWePH-3_DLBEtRs/p.png',
+        productName: 'Blue Bead - Cold Glaze chocolate flavored',
         productAvailability: 50,
         productDescription: {
             title: 'Test Product Description',
@@ -36,9 +48,21 @@ const product = [
     },
 
     {
-        productId: 1,
-        productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
+        productId: 5,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACj9r4K1CkdDdCnYyDkPfQ3NEdh0pWdULutB5g6HkAHSwOSXZCONkMxvhFdnu1GC1c5jmKNoolCWCsolyWz49mQCworBwG_EMoC2leVFJW21owAqUScqm3UfQwCUPbA-G3WW0q_95NudhxBANYekw7GmMIgtvoQjYnZNOivuoxbOSOhKkYu-lXgSkkGAszyUyaD_Y9neoujY9pUQcdYoXHcyw6xuInj3D4xyUww3Y8cLUpUpLMnvjDxtuC8gbS8lgVW_Yz3y779wi7Emzz8t4e_mLx-v3kMrANCUzS3FtP-wT3BhgMzv_DMK32qsWr9UdQE/p.png',
+        productName: 'Blue Bead - Cold Glaze chocolate flavored',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Blue Bead - Cold Glaze chocolate flavored',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 6,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACgOQNOcJQt35Y2EDwJ_Cj0KXsyegatlZITx9BVdKURwX37aI11yRQkw4PLJSM0PqD1x7VHl-O00ShtKZnf0h2KV5Vvxk7c_ECkSA4Bzok9mFi-cEJaH2TvUg1Uo_PGdMCBz5Ua6q89hn4bIlANWjoEfq4UwCNSyanVH_I_shLaQD-gJTS33gk_yH6-PCBvfWNnSkx7cfpg42RcKmZQf6jmCbnKdkHIwbUyHKVy3-QGAgJrou1waWKg4QppQZNYOM-4KWamQPYqeXCFO0rE8tvV256rKml3sZjZ0xgJuqfceTPFQGhyg4YRvRMLX8EcgW9E/p.png',
+        productName: 'Blue Bead - Cream patisserie',
         productAvailability: 50,
         productDescription: {
             title: 'Test Product Description',
@@ -48,9 +72,21 @@ const product = [
     },
 
     {
-        productId: 1,
-        productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
+        productId: 7,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACha9X1Ovmw1OyRFjVM1sAt1E_LwuYQQ1MtC0ZN22O5SrR4XYp_dM7UI_R8CxQQ4WpD8hbfgg7lve4tYD-nzIjRndWsRBfVj_TWZnJnKPi5byfkznqGp2cIxr333taPy4pG7X1iePiclbGXi14MERlF6Qe7Gbmp7VdsD5IfFKmL8YKJQY2hxh1y343fXAuaVbisUdRC2GDCHcN0FlLvT5qGf-hns6hwAgYT8WuF_DFxlXQWoo8q7LyxPxR2NNriHT65SCdikPbldTqMle8vUQ_g5B6JSOIpTOGiD9ROCTbcLTTnY7i_sy9u8vitbPG42OIM/p.png',
+        productName: 'Blue Bead - Pandispanya miksi',
+        productAvailability: 2,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 8,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACjHbgSHIGfUMEc5gcb_eZ-2E8_TN0iyz44vuKMzC5PwKgWVKq6kYjr8y0ReCWO3FXAJzLx_qFwsLoV5Vr9lv-J2T1AhDY6bslJwagW9XDuhfpKh7m8qQ9ZH5PtafuaJ8njxd87tS0o3Ge6Egicus_2tUrMKbVWo1Osmb-AcRvEccC9EYcpGsw0igai6l2dmLFGDo9qns8Z6zBfmpydnzAw9tSeFgEBIv-JK7a4fOIi3jV_tPXuOfCCrPc3-i-hBAkndUJ0HmHkS3U-pLJE5glPj_J1FOeyXKOiWVmXIwtQtAkzhiJiOoiwRNcS5gFZwe2M/p.png',
+        productName: 'Blue Bead - Pasta katkl maddesi',
         productAvailability: 50,
         productDescription: {
             title: 'Test Product Description',
@@ -60,9 +96,9 @@ const product = [
     },
 
     {
-        productId: 1,
-        productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
+        productId: 9,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACiUTarY5Vm1pKRw_tuHawIcbZ2Y9yR-dEQJOS1hpLRK3mDMgVkwuWgzqy-Eh-xCj3B5EAHv7w77qnTXtgUSBJGeDiSidWHH4tPzf_2mN7xCNu6qNT4agkC0xHpW09zl0URgHBagWvgHezHqXgPf89tsL7mjAqL8-CVsB-wnj4tgnYRzjcza4QuvwIZKpo-5tS4jQHRGXU3c5a7zD5Pbz8J1VCAL6BI2udEJXKG5pcYy_rT-22Xg_uFTe-h-M5VOYdMZkRwR_H0J3B5l_V7IGFnsYrzswcqaBFIu5Kzwoi0kuiFPWCiKJ0VGSZDwcNlV7dI/p.png',
+        productName: 'Blue Bead - Whipped cream powder',
         productAvailability: 50,
         productDescription: {
             title: 'Test Product Description',
@@ -72,22 +108,10 @@ const product = [
     },
 
     {
-        productId: 1,
-        productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
-        productAvailability: 50,
-        productDescription: {
-            title: 'Test Product Description',
-            content: 'This is a test product description.',
-            price: '950 ETB',
-        }
-    },
-
-    {
-        productId: 1,
-        productImage: 'https://previews.dropbox.com/p/thumb/AChabt7TKx8SoMNifwGYSY5id4d0hJ-NYOZK5o4zrDlWHt9RW7_QKt1YiFLKnN22H4PnnV9g44sulcqWBzf2sULvg6hzEsan6fZWU5EkILg96iT7aUE_llfhUXkaPnTh_bg2bqI5Tf2TneEmm5V2Z6msu94rDA0OfkaaDnT21ju_R0HjqvXwvhD40kKJmX-kanLoVCcbvQa0Owud6CCO2G1X9TPIJV6Eqf8ahyY78bePh4fIQseMudwCWUriKx4mEsbIB8vkKaqum-9kv_y5FcQeonfAzPpKfzRC91NdQVYl0oJLbjbCsEw3BMiVa9_qKbc/p.png',
-        productName: 'Test Product',
-        productAvailability: 50,
+        productId: 10,
+        productImage: 'https://previews.dropbox.com/p/thumb/AChzQZc_T4_uqMfQw6zL5s1FBTBzTqoGGvyt_HxV6IAb0W-J1N8YVY3f_SutSGxk8j2ertaKRcNVFRJuSnjMG6lBUsTcoGt7oc1I4NQ1WJUZGPxYyt7NSAyN5Bn66Ure48r3GbM0edWsGLJ4Yj8a2ILwGH1txkGSRw5N-qKj97twaRnax9Aa7nFjQtfe5nlElL34uvUn-cvImo66-yHSjo4IAEiP7Ufqk78KYhw4QYCovjVH_IiKH3CxlC7prwsKYELOaf8I1mjvjbem3JwuWU9Nx857OnPgyTzDlbq1a2Scfb4dtZtP2ajkRYYOCeljNUs/p.png',
+        productName: 'KataKit - Chocolate Lee',
+        productAvailability: 0,
         productDescription: {
             title: 'Test Product Description',
             content: 'This is a test product description.',
