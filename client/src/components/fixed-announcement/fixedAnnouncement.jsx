0fc2943639574a1065e6f3ad225a6b51c748569b
Prodaaron <aaronmesfin60@gmail.com>
2025-01-09 00:22:15 +0300

    about page layout and style

--- client/src/components/fixed-announcement/fixedAnnouncement.jsx
+++ client/src/components/fixed-announcement/fixedAnnouncement.jsx
@@ -5,7 +5,7 @@ const fixedAnnouncement = () => {
   return (
     <div className='announcement-container'>
       <h3>Note:</h3>
-      <p>All orders are currently being recieved  <a href="https://docs.google.com/forms/d/e/1FAIpQLSdbyZ7bOotGBjpxjs427SswTKYiG9hHnsOBVjbUVF1lsoPa8g/viewform?usp=header">here</a>, <br />We are currently working hard on improving our website! <br /> Orders through this website coming soon!</p>
+      <p>All orders are currently being recieved  <a href="https://docs.google.com/forms/d/e/1FAIpQLSdbyZ7bOotGBjpxjs427SswTKYiG9hHnsOBVjbUVF1lsoPa8g/viewform?usp=header" target='_blank'>here</a>, <br />We are currently working hard on improving our website! <br /> Orders through this website coming soon!</p>
     </div>
   )
 }
--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './css/about.css';
 
 const about = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='about-page'>
+      <div className="info-container">
+        <h1>About Us</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus tempore voluptatum omnis beatae minima maiores alias eos doloremque, laboriosam, aspernatur reiciendis animi! Aspernatur, natus earum! Quidem rerum cumque nam eligendi.</p>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quo atque nulla nobis reiciendis nihil corporis expedita consectetur velit voluptas, ducimus voluptatum ea molestias quae quibusdam fugiat debitis molestiae ex? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam porro suscipit in aliquid, consequatur incidunt illo eius eum minus magni dicta corrupti et omnis distinctio officia! Facere cum maxime similique!</p>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium dignissimos perferendis ullam aperiam nisi tempora, tenetur, laborum alias architecto possimus libero nesciunt similique quaerat ducimus esse magni est, qui minus.</p>
+      </div>
     </div>
   )
 }
