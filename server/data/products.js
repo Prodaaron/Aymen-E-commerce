f029e0aecca8d68cfff55fdc91737d605b538138
Prodaaron <aaronmesfin60@gmail.com>
2025-01-09 20:43:48 +0300

    Added multiple products

--- server/data/products.js
+++ server/data/products.js
@@ -117,6 +117,258 @@ const product = [
             content: 'This is a test product description.',
             price: '950 ETB',
         }
+    },
+
+    {
+        productId: 11,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACiKA9X59UQLSFjYzhRsabanD2f6YdbKi9wiqrDDci8VffCZ2jpYu7W4FRt12ilQUuSUTg0_gbsyYoxCsOtD70GutUHYbA3rKFdozwZJzLp2sFE31hduG8dsLQ6p9m1B9MgV6ZktbsnhFEldQ9JiuZaZ34wrOAn9Ey0XKjlr5siKDVX1tNXFoOyTBVQtEWexWX4FkhJkWKbtoXlx8sRBKNqSabULXbKyWc3cDj2-1D81pS2pgw_YEhceumShmmYlUxgfUpiY0rWQ0SPK_8O82EuK34l8kwSh4NEzAN_s-2aFQstaDsE_F2ELB7pOcU_T_eo/p.png',
+        productName: 'KataKit - Chocolate Lee Wt. 4KG',
+        productAvailability: 4,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 12,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACgfriUMwbhDFcyX5sHhSeI5U-SnDFl8X5qIlmPVwEm1sjyInUmRHx9KRP0LdJTUu99nKsLyc_zJr6Oug2-lAvQhkbNuLCEiM_D59yMxp5pmoAb7SpZ_uoNQ4jAptr-OAzJ8hWkcntfR6XcGDBJz7suDl38MyAjtUOxwFRPSx1HWgCSN_aU8mbdadEtLKfiyJQ14mVtAntXWV5aKZEAEhodHm_9vw3udkWD0gxhEHBtDdbCcDiDZJ8I_DFro-ZmwiEqu6iOsLP_ErVc5gJ8lHFGBEfE4bB_DOawFYzUsnEmmn0rfd_-iXdTWMVUmr2JfPRs/p.png',
+        productName: 'Kent - Chocolate syrup',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 13,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACjv_IQbsBORBnRxQkGa7a1HqpBSw62RI6MYpvWXNrc5mHfk9YQEKyoIoHk642wWLFI7FC2N6KpI1ncFxe8jeYzYV2gRP4FltLErAVk0P9ZNiO9XJ-vBM8lEbAlsIfGCqx1CJJHDkd8xDkB7BBYJH1ElaI7P-YyCO7J2sPboGKf9aMBcJlU9TCYtA4Ho_hvzydAGOvLOVxFo0j_dD-ydZ4omAysKSbjoFTISny1-_DkQH5e5a_XsPzbZ7a34Miy5azKXB2gsSgWBtE3Gkjiv18rv9CIW1SE-P9TG5sbYehGyiIFTvuf7I6CXJGAaLvqeLo4/p.png',
+        productName: 'Kent - Strawberry syrup',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 14,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACjv9HZ89H5gIPKFH53JMz7qpS9vWS3PjDBfav-YzpYSIWOFdSa1yfmBS1XKdxoXddyVhUXqqQIc-h8HySp3ufyM4I9RyAtuEofZNxtsK_ut7phPv1-q7BVstM4ezZdF4QB_ZawgX7a9DYHd3-Ivrgx5hQfw8NnfifBLT3IqXRyceP1Y4lDAyLjngrUCLZrlb2M0V6g0Xvu9-Poa3z-GqzrjPpnVn9KTryJXd1UHhtz7pLOHcVnfhBoj1b5MwdChXehcPY17lb7dI6-rIDtpCYuJ9mQgFHs-Qqd1rYUQEa8lf7OfL_9ttTwRJdSZlNbsq6g/p.png',
+        productName: 'Patchee - Chocolate cream paste',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 15,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACjLnUpnGW7K9CkQXG_WBW8TzmFt8snWHOF30due9BxNENZcpL0Mr4xQVS4JZgTGY1Xhi9nr3y-ZfeJkcc-_1e_5a-QYD89syCn6N328FjQRNKEvn7uWfoYap1ORF69_qTg0mzmtApIAt9KIugabKE8ee906RZ6TASZudZYmX-FEatJw170ZSqkuHhuzjz14iY9H71HyIDMbfRc6a-DkUEQon7NhqCbz1XzO-AFBTo1oHhYui5zGjZyI6bV1qtGYAt40FmerXhClwjBTlAaHmgIFlQPivKtgapRHOp-mHF-dbB7rnM9TclwGic9rxsQFuXQ/p.png',
+        productName: 'Patchee - White chocolate cream paste',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 16,
+        productImage: 'https://previews.dropbox.com/p/thumb/AChJwne8xks0ydJ-OLad_wj5x2kGA14KkKnTSizeLpC238yoHIStWFs9BnhSGVYj3C_2ZDUi1Nu0pR380QZvtpmdk3x8uf_ufxz-BU0JZV4xPpK2owNuFpOq9d06os56LV4-rJecLkqepjoiT-edKOp2BrwQduxWwqni4FN2cI4Z802Ve1BK2jKHlFfacAsad4PGLsn38QFN9pP33wpCZmjKf8Bod4j4F1mFGgKFL_uJ6uQbiEaovm1gifBhOIMalYZ5VQIRtLhf7GCRebTrEsYmiPBtSSv41q2I1MbGIwChlwHNKumCRSiNzt1xRBZEKS0/p.png',
+        productName: 'Pelik&kano - Whipped cream powder',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 17,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACgg2FDSoRzrYR0yz_zQRRB1WbeYk-r4InAqfl_WzcP9LoFvLZEChPXN_rUkBp5D75BXEt2Ftwtn7lcLd5VJLNQe9XHOmq_nXRC0D0-hO_1A74r9X58i2ujTk-JGTitaoTH8tnHH_UBQuy6lqZm00wd1JVL4X6k8dJO9h1PzyhZNzVbihI6PSevD_CaIb93VrQgejBEwbckQ9lQ0U61iCIdVqnnVyAGg4ghK4UIajx0HXZgc6XYNd2mgk_ni57jX8A4c1HDZ6Xt6ESnYHMQzupJXYEyB_30QN_LFc5_SlFxmh7VoblUh-O8MFuJ3fBlDugA/p.png',
+        productName: 'Powder cream patisserie',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 18,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACiwBFBNRCi4czCdFO98JVWBSYusLz_0BwPgNbsA_URHRg98B5wnXn34Oa0eIKSwKhxPxImPXGg0wQITMbUuCY0nWa0DFPdNaUpbQyCIl5VCcuNbJ1xg9b7QGKBx-anFZWNJI2mN1D68-Jbdcy31LXL1M1dacpbOrmuQ0JdFzWve_U9nxQcCEN8KEFY5GloCsc3QbfbLh8x5PoUKPwnqHy9WXGiP-tcx_5KY0yWSIMLHvWVZ-NcU-c4s_Jip29a4UhNyoT34zN4XOpky-WuS5orK0jyi53_5FpoHufRZDkH4Dh70mI1sN5cWL_X5E7Q3ceU/p.png',
+        productName: 'Puratos - Brillo caramel',
+        productAvailability: 3,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 19,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACh7_U3jiSSiKm1BShdUBO1KP2JfaryZt1JaeHtC7cCoSq-mcNiLH6FbXQ0lhLpnBNyLx_28scTBxNdbHqtjbD3hM52JxxvbhThfWVNF7BAUWvqCFcMcIFLDIFQE048CDztzvlw2U231qMgmSBGFHhOieU5LNx_sLtbZ9tDoRu0f9dshjamSaNvu04wivrExCxdPg7sHOoTjKDo7WMNX5BFfxrX6ijNxOOV1GuYlPbAncXhStTD-aPjZcy5dIS8MFmxK8SeZOOUpe2RWVSYhVMOHkqbAH9Vt4SBbVzDwGhPhBTZ920B6jnvwKnKVbwECWBU/p.png',
+        productName: 'Puratos - Brillo Choco',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 20,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACjc9Z6HRyV_bRA41_U9N6iwGv0mZpuuX_nloz4MzxaKdj_WGZEg65j_yer-Zz0a7Mrl4Z2uK_Gh6nj9LRTN_sS4XoAeHwglO9TJOXlxLeEdm-r3TQm8BsK-SpaWoY5kaT9t2bDduFJEwlExh2eUdE2eySsoGCpCHPYCC24wsS3vxrVLRdEqjfeMNRmzy02nF4ptgaIbt0EOw6OciF5k8Iw5gvHHm8-d5uXLSW_Gd185HPDMcGU5xpnKIvxrgmUzX8ZqRmnPwFMgDxO7cpPk8GLTWU9-0y3zEvK-V1sCM2A-c-ixYvYP_QekZSUc3XVJexk/p.png',
+        productName: 'Puratos - Brillo Strawberry',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 21,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACg7O-Fg44-6rGlGoeFS1QBC9djENWiUK2qSoT_Rlq_j1nOdM42KA1XxoVvdtus6fVB4_LyzjrfNuA5Z7VDoQydPqohSkL3Njg15QYVeQkKGA3CjZYIBvZACGrODiAe7Jad3R4Wf8NX7cf_COrMVKpFWkUEuu_x3c1ZDncNXXiuFYdORE0NnQ2l1wdxtC0xsfqvSxGIoUXnylD7JUTGLURFtkrHgHw-nu8nUROTm-d-TgPJ6IVoTyF29wF2i73MpWcO8_dPKgEX_sZWx3p6UhTuNPye4O0nNwweXlipICYEQxfU_Z13nqwbwS8QxLJUbXIo/p.png',
+        productName: 'Puratos - Diana whipped cream',
+        productAvailability: 0,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 22,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACiF5WRuPXAnhM7WO3Z6JqvAWvZ1q0-fB5e6bLs_iOuioccsSdJQ953SO7hnTRlvo31UebVQuUlCRWxduTXFX5F3nhc1zmlCVGWhiIBvSSaHzdRLAGOJQcmC2MGko-V8gO6ZIJPY-bkTG8nkuCAoBIQ8rd-HOhrnDo81RBsn_uBJLZr2p6J-vB6WvYgeQfIdE_BiiuD-vFYiExFvoEvEWPgb0S2-Nl3JH-UJS5QmrcfIyOJoDXstZA1K88tPQnoJVQuOorPas1WNegVY5XWgHKzbZebKlxUP7OPSpiHNtk_Iq-a2ENJFXe4_OOihc2g51JE/p.png',
+        productName: 'Puratos - Extraglace vanilla',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 23,
+        productImage: 'https://previews.dropbox.com/p/thumb/AChkuxAXh8lbrYdh6edaV5v1uuZtEC9_K7Ew7YTYGfa66-tmulmp-yJGEqdLKDwsKGyaFB8EhoUnBzC6mqIXxn0M5ZApSYKNkfZNVzPj2VfGPK3bOCfXBjd2eaLaL9bz9-AAkB13UGWzNis0HQqDKjzOKK67aWMYoG6sqnK4jwBVO2E8VUZL-GyW1509Wrp2BMmoEJOYpp814426A4eCpiNUObWHZXeDYeV753mQyehsel_YcysldR70BCMzXn9of69UV4TdQoXZSRi0svTMZcABixXqQbv6RLJ4WHEcS9t1zKp3blYuUlJgGBzmbXV0rVI/p.png',
+        productName: 'Puratos - Satin chocolate cream cake mix',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 24,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACgKLkAW9bnk8bMRhK_Mavbzcb84Ef9vtoOPlJiojpvhlkJgBPmOGXxyFpZeWjyUk49UOlSEYRDb6Z1sVnL3jTzt4jLEGtYpt0xzOiPbPUBwqkbLI3oUW_ulQAzGwrWkaBf5AbcuLaNymiKPmcdb5TUCpUNiivtVqCzT9y2CiRndBZvTfk59fcbadfVOaAtVJY05Yqgxqt9yrNOeVQaXD8QkDkSUSi_n2iQl8GFIl8LbOCgCSs6AmQBIQOgmSsqcyqsJdljAzbxL3_4IsrKdYwmHB_RDR2pTOIRlRL96khHnw6Mts_Jg-hbjm5XtXNtgKhY/p.png',
+        productName: 'Puratos - Satin red velvet cream cake mix',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 25,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACj31-Wj3wIaaCX9r3zpInDFwx6MfHyGfCG_cRzjojE22s4SFOoa426gA69a4Gjgo5rpoE6wDw2fUV1pYRYcYmDY57vp3yXW_TtEX5hMsrMkYDlwnDH5WD3eQlHb9dhMuU9jlUj6ZGObz5IfIIS-TPTL9zrwGaeo8MwEQEsmigWU2yjOcfCucpbK2KrLcYJXC4N7jskVjLM2rXwO9LtQfOVWn8iqvfqYWJPPFuI7SDRruuFB8B62Pn7hsBm0zR31bu_rWAKEkUBM3YbquGitpjNG_NHjWK_sQwFTkflLDxDSKk52fB_WGa-LYwxhjobacVY/p.png',
+        productName: 'Puratos - Satin vanilla cream cake mix',
+        productAvailability: 1,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 26,
+        productImage: 'https://previews.dropbox.com/p/thumb/AChK5hSQg5DiQt7sLuxmb9wQtpO9U0Dn4qDXQPJyVe6zICogpe8oyxOhinAPy16nfTyrgTiBCUWjUFyKFuZo_T2u3LB52pxGn6bARob4VulliY79hw7ETI4SLcnLEzguq8PSkoDGL1xMCZmsK5jbfryPkdu7MhnkyA_FBHNtUC4nvHAyfzOtBXlBFoApGFQP-wYzuLsXQ61Qb2wTrjli00l9X2WGGMk1cFttM2Fgnfp5jsMcOfSRCz1bAFKK_rIu1YoRTYcR9sCjCEovuq8pBBU2GBiXcq-WxShaP-IYOAzf-574XM9Ajwm_iFoVJ-mPkZ0/p.png',
+        productName: 'Puratos - White sugar paste',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 27,
+        productImage: 'https://previews.dropbox.com/p/thumb/AChGO9RhUX0N8bLTA_JEJCTwKxB1C_NPXnzlWXdzb3peZNuLfoLRjb-vrtT81zd-YDf6SLzI8a1417HQzWRG-yOscxkC0oTr9DGO2L1tzjyknEcsftetz9Ll4DIBZSqumw6yRXPockNFtKKAfFcKDz1gb52c3WIJtDjofvY1-S3fZKlOFc6yz11Jin7SejMGVPUi0tIEmiAKqlbGPN3PKEqUPRsY4yz6KZavDWnQDAPoJpcVDyQzP8mBAwsDyNTMvO_438SwcQxr2jCfbeaGyRg7p3W99XxkoRgpPlBYR4TpfIMKyTBivH7a1eewSVEfleM/p.png',
+        productName: 'Puratos - Supergato 5kg',
+        productAvailability: 0,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 28,
+        productImage: 'https://previews.dropbox.com/p/thumb/AChkhoV5Urot17dSnnqpHzsphV1ad8NcjO6il-_mAlnuDLTna82v6pNDJpo9bkzGWD68k-Hrfbf5N-W60R2tpY3qvouM0fkCmlaT63NcOm5syRbReuXlz8TEVGkqF0313n-0C5kSPrb8FNbrV-txXDV3QRVT0mfL7ViSOTO6Vi7SWcaVZwgOKQ1CBfy6JU8cjGuW0RoyE6drF66Rwc1Vjshz86JKvQA8iXfC7rO3MCpFvm0huUYyc03S9HuB3MVYXKI7ymic78-WJaNBbr3xN5IFEuEqxi1m7OPUdkYzHnSDQaBLige3kOt_qAorYdNvEf0/p.png',
+        productName: 'Puratos - Supergato 5kg',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 29,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACj2r8_6pt6B3vttGEysrUL1UxW_Y6JHymefoo0OJbnrfMOh8omnp3n1He_YpfIg3gbC5X91__1vGcUOZ8a3_U5Uu0DEidxArbA_bMsnUrmU8nMyDUs9se8oIf4XcMr-kjWE9C-9M1fNYHTmmdnMxEHgKa8Bhc7XjnrJhEEHeJsXI5B5zKt5w_lBIJj5UmSK0ZHUTAlqvWq3xXiyRet9NiG1IqgqTP31tNr7ndIn1L1ZsqL41cNVvLTlVowqLBO7DmWixDLCzUhrBGlN_7GsG2ZfpwJXTtOsFB9YGlclQWTv4xNlxJTFehyuGADP449xuzk/p.png',
+        productName: 'Puratos - Tegral Satin cream cake mix',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 30,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACiy7uOJb7GCauMgkHna-C_8J_CMwWOHbGH3XGBjjy5x7WsFfhm8mC-dFi8-vy4scIY_XQLBMC9hV3JunBnnK1cdUwuESktT1JGzmy8jFLZbxxgGwE6ZX657wyaw8BGE0r7zMfGRLeasd1Faf4myu103mNlpOZVI5RIS0v2qUr--_hKXcSOYuWxWLPLyW_3-_LXMg8J1nR0Ko9YburIb3HZARuPZxL4JupuZPLjlh5HMO_SFB8P3_0E31OT7iBnXk4n6hZ5gv_kNSmD5wVhHgEPGacEN73tQkeGrhs0rMum4VZziZnHPWYZDZu2661GOGRQ/p.png',
+        productName: 'Puratos - Tegral Satin chocolate cream cake mix',
+        productAvailability: 1,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
+    },
+
+    {
+        productId: 31,
+        productImage: 'https://previews.dropbox.com/p/thumb/ACjPeWLwCwv0Q3FH_xhtRSxOzS6zFJuKftoJiMKBBDomN1wQb3uni2k16r7dkgAJR6QlN53iu_F1893-4cC-sZaxe3HtNlSkWgK45uMyGdfCYtqJIXajeOWRYSp_wc-auhZ9Bq8xqJ5MBS2Pkq--EP0x7PYHsYoc3loOruuXxKiEXEmCbNZur5HueUDgxNcGIS9l2oUCVZbYqkLlIxE4ySrcYcDCsXgS8nDF_1KfzFjH1GTPNj2GQ70Ve3ObdMpx0JDF9YRILRuaMlOIPpTc5dtph_Sgyy1XOq_wvEDml-FBXpa8opKCemBwp7Oei1Knskk/p.png',
+        productName: 'Taz krem santi | whipped cream',
+        productAvailability: 50,
+        productDescription: {
+            title: 'Test Product Description',
+            content: 'This is a test product description.',
+            price: '950 ETB',
+        }
     }
 ];
 
